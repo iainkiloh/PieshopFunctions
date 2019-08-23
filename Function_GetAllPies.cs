f91fda442b0f4b9eed0b85ffd16bf10b65bdd3de
unknown <iainkiloh@gmail.com>
2019-08-23 08:04:52 +0100

    initial commit

---
+++ Function_GetAllPies.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PieshopFunctions.Contract;
+using PieshopFunctions.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PieshopFunctions
+{
+    public class GetAllPies
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public GetAllPies(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [FunctionName("GetAllPies")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/getallpies")] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var pies = await _appDbContext.Pies.ToListAsync();
+           
+            if (pies == null) { new NotFoundResult(); }
+
+            //convert to contract
+            var response = new List<PieContract>();
+            foreach (var pie in pies)
+            {
+                response.Add(new PieContract
+                {
+                     Id = pie.Id, ImageThumbnailUrl = pie.ImageThumbnailUrl, ImageUrl = pie.ImageUrl, IsInStock = pie.IsInStock, IsPieOfTheWeek = pie.IsPieOfTheWeek,
+                      LongDescription = pie.LongDescription, Name = pie.Name, Price = pie.Price, ShortDescription = pie.ShortDescription
+                });
+            }
+
+            return new OkObjectResult(response);
+
+        }
+    }
+}
+
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PieshopFunctions.Model;
+using System.IO;
+using System.Linq;
+
+[assembly: FunctionsStartup(typeof(PieshopFunctions.Startup))]
+namespace PieshopFunctions
+{
+    public class Startup : FunctionsStartup
+    {
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            //apparenty responses are compressed (Gzip) by default
+            //builder.Services.AddResponseCompression(options =>
+            //{
+            //    options.EnableForHttps = true;
+            //    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "imagejpeg" });
+            //});
+
+            //builder.Services.Configure<GzipCompressionProviderOptions>(
+            //    options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+
+            //setup EF Core context with SqlServer and the connection string to use
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DbConectionString"))
+            );
+
+           
+
+        }
+    }
+}
